81163d22616e186b27b9deddc4ac54572135ee4f
Joshua Williams <d.willjoshua@gmail.com>
2022-06-16 08:05:47 -0700

    Add project files.

---
+++ InventoryManager/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace InventoryManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.app_title = new System.Windows.Forms.Label();
+            this.settingsBtn = new System.Windows.Forms.Button();
+            this.addBtn = new System.Windows.Forms.Button();
+            this.removeBtn = new System.Windows.Forms.Button();
+            this.sortBtn = new System.Windows.Forms.Button();
+            this.filterBtn = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.listBox4 = new System.Windows.Forms.ListBox();
+            this.listBox5 = new System.Windows.Forms.ListBox();
+            this.listBox6 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // app_title
+            // 
+            this.app_title.AutoSize = true;
+            this.app_title.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.app_title.Location = new System.Drawing.Point(50, 29);
+            this.app_title.Name = "app_title";
+            this.app_title.Size = new System.Drawing.Size(340, 29);
+            this.app_title.TabIndex = 0;
+            this.app_title.Text = "Inventory Management System";
+            // 
+            // settingsBtn
+            // 
+            this.settingsBtn.Location = new System.Drawing.Point(640, 29);
+            this.settingsBtn.Name = "settingsBtn";
+            this.settingsBtn.Size = new System.Drawing.Size(94, 93);
+            this.settingsBtn.TabIndex = 1;
+            this.settingsBtn.Text = "Settings";
+            this.settingsBtn.UseVisualStyleBackColor = true;
+            // 
+            // addBtn
+            // 
+            this.addBtn.Location = new System.Drawing.Point(40, 75);
+            this.addBtn.Name = "addBtn";
+            this.addBtn.Size = new System.Drawing.Size(101, 47);
+            this.addBtn.TabIndex = 3;
+            this.addBtn.Text = "Add Item";
+            this.addBtn.UseVisualStyleBackColor = true;
+            // 
+            // removeBtn
+            // 
+            this.removeBtn.Location = new System.Drawing.Point(134, 75);
+            this.removeBtn.Name = "removeBtn";
+            this.removeBtn.Size = new System.Drawing.Size(115, 47);
+            this.removeBtn.TabIndex = 4;
+            this.removeBtn.Text = "Remove Item";
+            this.removeBtn.UseVisualStyleBackColor = true;
+            // 
+            // sortBtn
+            // 
+            this.sortBtn.Location = new System.Drawing.Point(273, 75);
+            this.sortBtn.Name = "sortBtn";
+            this.sortBtn.Size = new System.Drawing.Size(101, 47);
+            this.sortBtn.TabIndex = 5;
+            this.sortBtn.Text = "Sort By";
+            this.sortBtn.UseVisualStyleBackColor = true;
+            // 
+            // filterBtn
+            // 
+            this.filterBtn.Location = new System.Drawing.Point(398, 75);
+            this.filterBtn.Name = "filterBtn";
+            this.filterBtn.Size = new System.Drawing.Size(101, 47);
+            this.filterBtn.TabIndex = 6;
+            this.filterBtn.Text = "Filter";
+            this.filterBtn.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(40, 188);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(123, 264);
+            this.listBox1.TabIndex = 7;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 20;
+            this.listBox2.Location = new System.Drawing.Point(159, 188);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(123, 264);
+            this.listBox2.TabIndex = 8;
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 20;
+            this.listBox3.Location = new System.Drawing.Point(276, 188);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(114, 264);
+            this.listBox3.TabIndex = 9;
+            // 
+            // listBox4
+            // 
+            this.listBox4.FormattingEnabled = true;
+            this.listBox4.ItemHeight = 20;
+            this.listBox4.Location = new System.Drawing.Point(387, 188);
+            this.listBox4.Name = "listBox4";
+            this.listBox4.Size = new System.Drawing.Size(121, 264);
+            this.listBox4.TabIndex = 10;
+            // 
+            // listBox5
+            // 
+            this.listBox5.FormattingEnabled = true;
+            this.listBox5.ItemHeight = 20;
+            this.listBox5.Location = new System.Drawing.Point(504, 188);
+            this.listBox5.Name = "listBox5";
+            this.listBox5.Size = new System.Drawing.Size(118, 264);
+            this.listBox5.TabIndex = 11;
+            // 
+            // listBox6
+            // 
+            this.listBox6.FormattingEnabled = true;
+            this.listBox6.ItemHeight = 20;
+            this.listBox6.Location = new System.Drawing.Point(616, 188);
+            this.listBox6.Name = "listBox6";
+            this.listBox6.Size = new System.Drawing.Size(118, 264);
+            this.listBox6.TabIndex = 12;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 152);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 30);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Header 1";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(781, 705);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox6);
+            this.Controls.Add(this.listBox5);
+            this.Controls.Add(this.listBox4);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.filterBtn);
+            this.Controls.Add(this.sortBtn);
+            this.Controls.Add(this.removeBtn);
+            this.Controls.Add(this.addBtn);
+            this.Controls.Add(this.settingsBtn);
+            this.Controls.Add(this.app_title);
+            this.Name = "Form1";
+            this.Text = "IMS";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label app_title;
+        private System.Windows.Forms.Button settingsBtn;
+        private System.Windows.Forms.Button addBtn;
+        private System.Windows.Forms.Button removeBtn;
+        private System.Windows.Forms.Button sortBtn;
+        private System.Windows.Forms.Button filterBtn;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.ListBox listBox4;
+        private System.Windows.Forms.ListBox listBox5;
+        private System.Windows.Forms.ListBox listBox6;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
