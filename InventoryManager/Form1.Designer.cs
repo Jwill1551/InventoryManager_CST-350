064f4af2fd359ede06d5fce89e40a5ecf7ffa921
Joshua Williams <d.willjoshua@gmail.com>
2022-06-16 08:05:47 -0700

    Worked on UI design and created github repo.

---
+++ InventoryManager/Form1.Designer.cs
@@ -0,0 +1,398 @@
+﻿namespace InventoryManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.appTitle = new System.Windows.Forms.Label();
+            this.settingsBtn = new System.Windows.Forms.Button();
+            this.addBtn = new System.Windows.Forms.Button();
+            this.removeBtn = new System.Windows.Forms.Button();
+            this.sortBtn = new System.Windows.Forms.Button();
+            this.filterBtn = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.listBox4 = new System.Windows.Forms.ListBox();
+            this.listBox5 = new System.Windows.Forms.ListBox();
+            this.listBox6 = new System.Windows.Forms.ListBox();
+            this.headerLabel1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.applyBtn = new System.Windows.Forms.Button();
+            this.headerLabel2 = new System.Windows.Forms.Label();
+            this.headerLabel3 = new System.Windows.Forms.Label();
+            this.headerLabel4 = new System.Windows.Forms.Label();
+            this.headerLabel5 = new System.Windows.Forms.Label();
+            this.headerLabel6 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // appTitle
+            // 
+            this.appTitle.AutoSize = true;
+            this.appTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.appTitle.Location = new System.Drawing.Point(50, 29);
+            this.appTitle.Name = "appTitle";
+            this.appTitle.Size = new System.Drawing.Size(340, 29);
+            this.appTitle.TabIndex = 0;
+            this.appTitle.Text = "Inventory Management System";
+            // 
+            // settingsBtn
+            // 
+            this.settingsBtn.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.settingsBtn.FlatAppearance.BorderSize = 2;
+            this.settingsBtn.Location = new System.Drawing.Point(671, 29);
+            this.settingsBtn.Name = "settingsBtn";
+            this.settingsBtn.Size = new System.Drawing.Size(94, 93);
+            this.settingsBtn.TabIndex = 1;
+            this.settingsBtn.Text = "Settings";
+            this.settingsBtn.UseVisualStyleBackColor = true;
+            // 
+            // addBtn
+            // 
+            this.addBtn.Location = new System.Drawing.Point(40, 75);
+            this.addBtn.Name = "addBtn";
+            this.addBtn.Size = new System.Drawing.Size(101, 47);
+            this.addBtn.TabIndex = 3;
+            this.addBtn.Text = "Add Item";
+            this.addBtn.UseVisualStyleBackColor = true;
+            // 
+            // removeBtn
+            // 
+            this.removeBtn.Location = new System.Drawing.Point(138, 75);
+            this.removeBtn.Name = "removeBtn";
+            this.removeBtn.Size = new System.Drawing.Size(115, 47);
+            this.removeBtn.TabIndex = 4;
+            this.removeBtn.Text = "Remove Item";
+            this.removeBtn.UseVisualStyleBackColor = true;
+            // 
+            // sortBtn
+            // 
+            this.sortBtn.Location = new System.Drawing.Point(273, 75);
+            this.sortBtn.Name = "sortBtn";
+            this.sortBtn.Size = new System.Drawing.Size(101, 47);
+            this.sortBtn.TabIndex = 5;
+            this.sortBtn.Text = "Sort By";
+            this.sortBtn.UseVisualStyleBackColor = true;
+            // 
+            // filterBtn
+            // 
+            this.filterBtn.Location = new System.Drawing.Point(398, 75);
+            this.filterBtn.Name = "filterBtn";
+            this.filterBtn.Size = new System.Drawing.Size(101, 47);
+            this.filterBtn.TabIndex = 6;
+            this.filterBtn.Text = "Filter";
+            this.filterBtn.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(40, 188);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(113, 264);
+            this.listBox1.TabIndex = 7;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 20;
+            this.listBox2.Location = new System.Drawing.Point(159, 188);
+            this.listBox2.Margin = new System.Windows.Forms.Padding(0, 3, 0, 3);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(111, 264);
+            this.listBox2.TabIndex = 8;
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 20;
+            this.listBox3.Location = new System.Drawing.Point(276, 188);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(114, 264);
+            this.listBox3.TabIndex = 9;
+            // 
+            // listBox4
+            // 
+            this.listBox4.FormattingEnabled = true;
+            this.listBox4.ItemHeight = 20;
+            this.listBox4.Location = new System.Drawing.Point(396, 188);
+            this.listBox4.Name = "listBox4";
+            this.listBox4.Size = new System.Drawing.Size(121, 264);
+            this.listBox4.TabIndex = 10;
+            // 
+            // listBox5
+            // 
+            this.listBox5.FormattingEnabled = true;
+            this.listBox5.ItemHeight = 20;
+            this.listBox5.Location = new System.Drawing.Point(523, 188);
+            this.listBox5.Name = "listBox5";
+            this.listBox5.Size = new System.Drawing.Size(118, 264);
+            this.listBox5.TabIndex = 11;
+            // 
+            // listBox6
+            // 
+            this.listBox6.FormattingEnabled = true;
+            this.listBox6.ItemHeight = 20;
+            this.listBox6.Location = new System.Drawing.Point(647, 188);
+            this.listBox6.Name = "listBox6";
+            this.listBox6.Size = new System.Drawing.Size(118, 264);
+            this.listBox6.TabIndex = 12;
+            // 
+            // headerLabel1
+            // 
+            this.headerLabel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.headerLabel1.AutoSize = true;
+            this.headerLabel1.Location = new System.Drawing.Point(66, 165);
+            this.headerLabel1.Name = "headerLabel1";
+            this.headerLabel1.Size = new System.Drawing.Size(75, 20);
+            this.headerLabel1.TabIndex = 13;
+            this.headerLabel1.Text = "Header 1";
+            this.headerLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LightPink;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(40, 449);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(113, 47);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Add Input";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.LightPink;
+            this.button2.Location = new System.Drawing.Point(159, 449);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(111, 47);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "Add Input";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.LightPink;
+            this.button3.Location = new System.Drawing.Point(276, 449);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(114, 47);
+            this.button3.TabIndex = 16;
+            this.button3.Text = "Add Input";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.LightPink;
+            this.button4.Location = new System.Drawing.Point(396, 449);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(121, 47);
+            this.button4.TabIndex = 17;
+            this.button4.Text = "Add Input";
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.LightPink;
+            this.button5.Location = new System.Drawing.Point(523, 449);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(118, 47);
+            this.button5.TabIndex = 18;
+            this.button5.Text = "Add Input";
+            this.button5.UseVisualStyleBackColor = false;
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.LightPink;
+            this.button6.Location = new System.Drawing.Point(647, 449);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(118, 47);
+            this.button6.TabIndex = 19;
+            this.button6.Text = "Add Input";
+            this.button6.UseVisualStyleBackColor = false;
+            // 
+            // applyBtn
+            // 
+            this.applyBtn.Location = new System.Drawing.Point(592, 523);
+            this.applyBtn.Name = "applyBtn";
+            this.applyBtn.Size = new System.Drawing.Size(173, 48);
+            this.applyBtn.TabIndex = 20;
+            this.applyBtn.Text = "Apply Changes";
+            this.applyBtn.UseVisualStyleBackColor = true;
+            // 
+            // headerLabel2
+            // 
+            this.headerLabel2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.headerLabel2.AutoSize = true;
+            this.headerLabel2.Location = new System.Drawing.Point(178, 165);
+            this.headerLabel2.Name = "headerLabel2";
+            this.headerLabel2.Size = new System.Drawing.Size(75, 20);
+            this.headerLabel2.TabIndex = 21;
+            this.headerLabel2.Text = "Header 2";
+            this.headerLabel2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // headerLabel3
+            // 
+            this.headerLabel3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.headerLabel3.AutoSize = true;
+            this.headerLabel3.Location = new System.Drawing.Point(302, 165);
+            this.headerLabel3.Name = "headerLabel3";
+            this.headerLabel3.Size = new System.Drawing.Size(75, 20);
+            this.headerLabel3.TabIndex = 22;
+            this.headerLabel3.Text = "Header 3";
+            this.headerLabel3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // headerLabel4
+            // 
+            this.headerLabel4.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.headerLabel4.AutoSize = true;
+            this.headerLabel4.Location = new System.Drawing.Point(421, 165);
+            this.headerLabel4.Name = "headerLabel4";
+            this.headerLabel4.Size = new System.Drawing.Size(75, 20);
+            this.headerLabel4.TabIndex = 23;
+            this.headerLabel4.Text = "Header 4";
+            this.headerLabel4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // headerLabel5
+            // 
+            this.headerLabel5.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.headerLabel5.AutoSize = true;
+            this.headerLabel5.Location = new System.Drawing.Point(542, 165);
+            this.headerLabel5.Name = "headerLabel5";
+            this.headerLabel5.Size = new System.Drawing.Size(75, 20);
+            this.headerLabel5.TabIndex = 24;
+            this.headerLabel5.Text = "Header 5";
+            this.headerLabel5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // headerLabel6
+            // 
+            this.headerLabel6.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.headerLabel6.AutoSize = true;
+            this.headerLabel6.Location = new System.Drawing.Point(667, 165);
+            this.headerLabel6.Name = "headerLabel6";
+            this.headerLabel6.Size = new System.Drawing.Size(75, 20);
+            this.headerLabel6.TabIndex = 25;
+            this.headerLabel6.Text = "Header 6";
+            this.headerLabel6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(12, 188);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(21, 20);
+            this.radioButton1.TabIndex = 26;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.ClientSize = new System.Drawing.Size(799, 587);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.headerLabel6);
+            this.Controls.Add(this.headerLabel5);
+            this.Controls.Add(this.headerLabel4);
+            this.Controls.Add(this.headerLabel3);
+            this.Controls.Add(this.headerLabel2);
+            this.Controls.Add(this.applyBtn);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.headerLabel1);
+            this.Controls.Add(this.listBox6);
+            this.Controls.Add(this.listBox5);
+            this.Controls.Add(this.listBox4);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.filterBtn);
+            this.Controls.Add(this.sortBtn);
+            this.Controls.Add(this.removeBtn);
+            this.Controls.Add(this.addBtn);
+            this.Controls.Add(this.settingsBtn);
+            this.Controls.Add(this.appTitle);
+            this.Name = "Form1";
+            this.Text = "IMS";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label appTitle;
+        private System.Windows.Forms.Button settingsBtn;
+        private System.Windows.Forms.Button addBtn;
+        private System.Windows.Forms.Button removeBtn;
+        private System.Windows.Forms.Button sortBtn;
+        private System.Windows.Forms.Button filterBtn;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.ListBox listBox4;
+        private System.Windows.Forms.ListBox listBox5;
+        private System.Windows.Forms.ListBox listBox6;
+        private System.Windows.Forms.Label headerLabel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button applyBtn;
+        private System.Windows.Forms.Label headerLabel2;
+        private System.Windows.Forms.Label headerLabel3;
+        private System.Windows.Forms.Label headerLabel4;
+        private System.Windows.Forms.Label headerLabel5;
+        private System.Windows.Forms.Label headerLabel6;
+        private System.Windows.Forms.RadioButton radioButton1;
+    }
+}
+
